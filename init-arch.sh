31cd311fd30751a0aa6aaadf48088ca7c1edc296
wufe8 <wufeo8@gmail.com>
2021-01-07 19:26:19 +0800

    解决i3音量调整无上限的问题, 添加了更多常用软件

---
+++ init-arch.sh
@@ -0,0 +1,12 @@
+sudo pacman -Syyu
+sudo pacman -S base-devel yay \
+vim neovim code git python2 python3 nodejs npm gcc gdb cmake ctags jdk8-openjdk arm-none-eabi-gcc arm-none-eabi- libopencm3 stlink \
+ibus ibus-rime xf86-input-wacom xclip screenfetch neofetch \
+thefuck ranger wqy-bitmapfont wqy-zenhei ffmpeg mplayer vlc xorg-xev fbterm htop screenkey \
+gpart gparted mtools arandr \
+wine \
+i3 dmenu xcompmgr unclutter \
+feh spectacle dolphin google-chrome deepin-wine-tim obs volumeicon 
+
+yay -S google-chrome
+echo [WRONG] if using KDE, DON\'T change default exporler into chrome!!!
---
+++ init-debain.sh
@@ -0,0 +1,11 @@
+sudo apt install \
+vim neovim git \
+python2 python3 python3-pip nodejs npm \
+gcc gdb cmake ctags \
+xclip neofetch thefuck ranger fbterm htop\
+ffmpeg mplayer vlc \
+gpart gparted \
+wine \
+i3 dmenu xcompmgr feh unclutter
+#epiphany-browser
+#sudo apt install ibus ibus-rime
